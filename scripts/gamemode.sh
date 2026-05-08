f7f67b5f5bca2a17ccb6b8e24910a3d6aa3a3064
al1h3n <al1h3n@molni.com>
2026-05-08 21:58:28 +0500

    Change

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -1,3 +1,6 @@
+# Scale 1 is used to fix pixelated look in applications and games.
+
+ACTIVE_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
@@ -9,7 +12,8 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+        keyword monitor $ACTIVE_MONITOR,highres@highrr,auto,1"
     hyprctl notify 1 3000 "rgb(40a02b)" " Gamemode [ON]"
     exit
 else

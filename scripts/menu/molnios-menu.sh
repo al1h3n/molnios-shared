20a8f359811e177bcc1cf22bfada6270d0221845
al1h3n <al1h3n@molni.com>
2026-07-19 13:06:50 +0500

    Fix

--- scripts/menu/molnios-menu.sh
+++ scripts/menu/molnios-menu.sh
@@ -119,7 +119,8 @@ SHELL_INPUT_EOF
     rm -f "$output_file"
 
     local term_cmd=""
-    if command -v wezterm&>/dev/null;then term_cmd="wezterm --config-file $L_PATH/config/wezterm/wezterm.lua start --class floating --"
+    if command -v wezterm&>/dev/null;then term_cmd="wezterm start --class floating --"
+    # "--config-file $L_PATH/config/wezterm/wezterm.lua" removed for Noctalia colorscheme.
     elif command -v kitty&>/dev/null;then term_cmd="kitty -c $L_PATH/config/kitty/kitty.conf --class floating -e"
     elif command -v ghostty&>/dev/null;then term_cmd="ghostty -e"
     elif command -v alacritty&>/dev/null;then term_cmd="alacritty -e"

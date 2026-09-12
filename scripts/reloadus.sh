b15faef31b3d290116532d2ad75ac056108e44d0
al1h3n <al1h3n@molni.com>
2026-09-12 23:34:59 +0500

    Fix

--- scripts/reloadus.sh
+++ scripts/reloadus.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-# MM    MM              dd           bb                         lll  1  hh      333333
-# MMM  MMM   aa aa      dd   eee     bb      yy   yy      aa aa lll 111 hh         3333 nn nnn
-# MM MM MM  aa aaa  dddddd ee   e    bbbbbb  yy   yy     aa aaa lll  11 hhhhhh    3333  nnn  nn
-# MM    MM aa  aaa dd   dd eeeee     bb   bb  yyyyyy    aa  aaa lll  11 hh   hh     333 nn   nn
-# MM    MM  aaa aa  dddddd  eeeee    bbbbbb       yy     aaa aa lll 111 hh   hh 333333  nn   nn
-#                                             yyyyy
-# Support - al1h3n(tg,ds) | Donate me - paypal.me/al1h3n
 # Reloadus - based on hypreload.sh project.
 # ==============================================================================
 
@@ -176,7 +169,7 @@ fi
 if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ];then
 	hyprctl reload &>/dev/null
 	reloaded hyprland
-	if exists snappy-switcher;then
+	if installed snappy-switcher;then
 		kp snappy-switcher
 		run snappy-switcher --daemon -c "$CONF/snappy.ini"
 		reloaded snappy-switcher
